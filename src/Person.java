f7731e7352a6b5ebcfb413121fb0460a1d7f2f43
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-25 16:54:43 -0500

    Person Object exercises

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+
+    private String name;
+    private int age;
+
+
+
+    // returns the person's name
+    public String getName(){
+        return name;
+    }
+
+    // changes the name property to the passed value
+    public void setName(String name) {
+        this.name = name;
+    }
+    // prints a message to the console using the person's name
+    public void sayHello() {
+        System.out.printf("Hello from %s", name);
+    }
+
+    public Person (String name) {
+        this.name = name;
+}
+
+public static void main(String[] args) {
+    Person person = new Person("Mitchel");
+
+    Person person1 = new Person("John");
+    Person person2 = person1;
+    System.out.println(person1.getName());
+    System.out.println(person2.getName());
+    person2.setName("Jane");
+    System.out.println(person1.getName());
+    System.out.println(person2.getName());
+
+    person.sayHello();
+
+
+}
+
+
+
+}
+
