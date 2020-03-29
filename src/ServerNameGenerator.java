9c98d1992717300f2b3dbdc4bee2cfef5e3a890a
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-29 17:31:42 -0500

    finished second exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class ServerNameGenerator {
 
-    String[] nouns = {"Johnny", "Orlando", "Bootstrap", "Cube", "Emily", "Timbuktu", "Sock", "River", "Wolf", "Feather"};
+    public static String randomString(String [] array) {
 
-    String[]
+        int randomNum = (int) Math.floor(Math.random() * array.length);
+        String randomArray = array[randomNum];
+
+        return randomArray;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] adjectives = {"Jealous", "Old", "Spotless", "Stormy", "Lucky", "Clever", "Unusual", "Puzzled", "Victorious", "Modern"};
+        String[] nouns = {"Johnny", "Orlando", "Bootstrap", "Cube", "Emily", "Timbuktu", "Sock", "River", "Wolf", "Feather"};
+
+
+        boolean cont = true;
+        do {
+            System.out.println("\n\tYour server name is: " + randomString(adjectives) + " " + randomString(nouns));
+
+            System.out.println("\tGet another name? y/n");
+            if (sc.nextLine().equalsIgnoreCase("n")) {
+                cont = false;
+            }
+        } while (cont);
+    }
 
 
 }
