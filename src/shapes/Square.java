5d3b627a00c93229dd327781738af9b2b9896018
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-31 09:16:58 -0500

    added overides in polymorphism

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -6,4 +6,17 @@ public class Square extends Rectangle {
     public Square(int side) {
         super(side, side);
     }
+
+
+    @Override
+    public int getArea() {
+        System.out.println("In Square");
+        return length * length;
+    }
+    @Override
+    public int getPerimeter() {
+        System.out.println("In Square");
+        return length * 4;
+    }
+
 }
