a3c79bbb38eec53ba85e34f2a145a30102f5192b
Mitchel Sandidge <masandidge12@gmail.com>
2020-05-04 23:56:32 -0500

    Practice whiteboarding

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,47 +1,77 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.lang.reflect.Array;
-
 public class Warmup {
 
 
-    public static void loop(int lrg) {
-        int i;
-        for (i = 0; i < lrg; i++) {
-            System.out.println(i);
+//    public static void loop(int lrg) {
+//        int i;
+//        for (i = 0; i < lrg; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//
+//
+//
+//    public static boolean isPalindrome(String str)
+//    {
+//        int i = 0, x = str.length() - 1;
+//        while (i < x) {
+//            if (str.charAt(i) != str.charAt(x))
+//                return false;
+//            i++;
+//            x--;
+//        }
+//        return true;
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//        System.out.println("hello");
+//        loop(20);
+//
+//        System.out.println("This is a test");
+//
+//        System.out.println(isPalindrome("helleh"));
+//
+//
+//
+//    }
+
+
+    public static void fizzBuzz() {
+        for (int i = 0; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 2 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 2 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+
         }
     }
 
 
 
-    public static boolean isPalindrome(String str)
-    {
-        int i = 0, x = str.length() - 1;
-        while (i < x) {
-            if (str.charAt(i) != str.charAt(x))
-                return false;
-            i++;
-            x--;
+    public static boolean isAdmin(String input) {
+        boolean isAdmin;
+
+        if (input.equalsIgnoreCase("iamtheadmin@gmail.com")) {
+            isAdmin = true;
+        } else {
+            isAdmin = false;
         }
-        return true;
+        return isAdmin;
     }
-    
-    
-    
+
 
     public static void main(String[] args) {
-        System.out.println("hello");
-        loop(20);
-
-        System.out.println("This is a test");
-
-        System.out.println(isPalindrome("helleh"));
-        
-        
-        
+        fizzBuzz();
+        System.out.println(isAdmin("notAdmin@gmail.com"));
     }
-
-    
     
     
 
