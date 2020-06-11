a9d3a920bb41d27df4e8f5bb32da64b4a335452e
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-11 17:03:59 -0500

    Warmups

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,41 +1,41 @@
 public class Warmup {
 
 
-//    public static void loop(int lrg) {
-//        int i;
-//        for (i = 0; i < lrg; i++) {
-//            System.out.println(i);
-//        }
-//    }
-//
-//
-//
-//    public static boolean isPalindrome(String str)
-//    {
-//        int i = 0, x = str.length() - 1;
-//        while (i < x) {
-//            if (str.charAt(i) != str.charAt(x))
-//                return false;
-//            i++;
-//            x--;
-//        }
-//        return true;
-//    }
+    public static void loop(int lrg) {
+        int i;
+        for (i = 0; i < lrg; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+
+    public static boolean isPalindrome(String str)
+    {
+        int i = 0, x = str.length() - 1;
+        while (i < x) {
+            if (str.charAt(i) != str.charAt(x))
+                return false;
+            i++;
+            x--;
+        }
+        return true;
+    }
 
 
 
 
-//    public static void main(String[] args) {
-//        System.out.println("hello");
-//        loop(20);
-//
-//        System.out.println("This is a test");
-//
-//        System.out.println(isPalindrome("helleh"));
-//
-//
-//
-//    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+        loop(20);
+
+        System.out.println("This is a test");
+
+        System.out.println(isPalindrome("helleh"));
+
+
+
+    }
 
 
     public static void fizzBuzz() {
