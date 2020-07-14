32c87e36f446ac5697a284a306f963592fb75867
Mitchel Sandidge <masandidge12@gmail.com>
2020-07-14 15:25:53 -0500

    Practiced FizzBuzz before interview

--- src/Warmup.java
+++ src/Warmup.java
@@ -73,22 +73,42 @@ public class Warmup {
 
 //    }
 
-
     public static void fizzBuzz() {
-        for (int i = 0; i <= 100; i++) {
-
-            if (i % 3 == 0 && i % 2 == 0) {
+        for( int i = 0; i <= 100; i++ ) {
+            if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if ( i % 5 == 0 ) {
                 System.out.println("Buzz");
-            } else if (i % 2 == 0) {
+            } else if ( i % 3 == 0 ) {
                 System.out.println("Fizz");
             } else {
                 System.out.println(i);
             }
-
         }
     }
+
+
+
+
+
+
+
+
+//    public static void fizzBuzz() {
+//        for (int i = 0; i <= 100; i++) {
+//
+//            if (i % 3 == 0 && i % 2 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 2 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//
+//        }
+//    }
 
 
 
